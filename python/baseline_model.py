fixed word embeddings, and calculation of hashtag embeddings by combining word embeddings

python/baseline_model.py
@@ -2,25 +2,36 @@
 import tensorflow as tf, numpy as np
 
 class BaselineModel(object):
-    def __init__(self, tweets, hashtags, hashtagMap, wordEmbeddings, wordMap, vocabSize, embeddingDim, numHashtags):
+    def __init__(self, tweets, hashtags, hashtagMap, gloveEmbeddings, gloveSize, wordMap, vocabSize, embeddingDim, numHashtags):
         self.tweets = tweets
         self.hashtags = hashtags
         self.hashtagMap = hashtagMap
         self.numHashtags = numHashtags
+        self.embeddingDim = embeddingDim
+        self.gloveSize = gloveSize
         self.wordMap = wordMap
 
-        self.wordEmbeddings = tf.get_variable(name="wordEmbeddings", shape=[vocabSize, embeddingDim], tf.constant_initializer(wordEmbeddings), trainable=False)
-        self.hashtagEmbeddings = tf.get_variable(name="hashtagEmbeddings", shape=[numHashtags, embeddingDim], tf.constant_initializer([trainHashtag(h) for h in hashtags[:numHashtags]])), trainable=False, name="hashtagEmbeddings")
+        print("Converting Glove embeddings to tensor")
+        self.wordEmbeddings = tf.Variable(tf.constant(0.0, shape=[gloveSize, embeddingDim]), trainable=False, name="wordEmbeddings")
+        glovePlaceholder = tf.placeholder(tf.float32, [gloveSize, embeddingDim])
+        gloveInit = self.wordEmbeddings.assign(glovePlaceholder)
+
+        self.sess = tf.Session()
+        with self.sess.as_default():
+            #TODO fix word embeddings not being initalized properly
+            self.sess.run(gloveInit, feed_dict={glovePlaceholder: gloveEmbeddings})
+            print("Embedding {} most common hashtags".format(self.numHashtags))
+            hashtag_embed_vector = [self.trainHashtag(h) for h in self.hashtags[:self.numHashtags]]
+            self.hashtagEmbeddings = tf.stack(hashtag_embed_vector).eval()
+
     def tweetEmbedding(self, tweet):
-        words = tweet.split()
-        def findInTable(word):
-            return self.wordMap[word] if word in self.wordMap.keys() else -1
-        word_ids = tf.convert_to_tensor([findInTable(w) for w in words], dtype=tf.int32)
+        word_ids = [self.wordMap[word] for word in tweet.split() if word in self.wordMap.keys()]
+        if word_ids == []:
+            return tf.zeros((25))
         embedded_words = tf.nn.embedding_lookup(self.wordEmbeddings, word_ids)
-        tweet_embedding = tf.concat(0, embedded_words)
-        return tweet_embedding
+        return tf.reduce_sum(embedded_words, 0)
     def trainHashtag(self, hashtag):
-        tweet_embeddings = [tweetEmbedding(t) for t in self.hashtagMap[hashtag]]
-        return tf.concat(0, tweet_embeddings)
+        tweet_embeddings = tf.stack([self.tweetEmbedding(t) for t in self.hashtagMap[hashtag]])
+        return tf.reduce_sum(tweet_embeddings, 0)
     #feed embeddings through covnet and feed forward
     #backprop to predict hashtags

python/main.py
@@ -1,6 +1,9 @@
 # Shishir Tandale
 import json, re, sys, numpy as np
-# import baseline_model
+import baseline_model as bm
+
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 class Hashtag(object):
     hashtag_map = None
@@ -25,11 +28,12 @@ class TwitterJSONParse(object):
         self.tweetObjs = [self.progress(json.loads(line)) for line in jsontxt]
     def progress_init(self, message):
         self.progress_n = 0.
+        self.progress_step = 100./self.numTweets
         self.progress_message = message
     def progress(self, data=None):
-        self.progress_n += 100./self.numTweets
+        self.progress_n += self.progress_step
         if self.progress_n < 100:
-            sys.stdout.write('\r{0}: {1}%'.format(self.progress_message, round(self.progress_n,2)))
+            sys.stdout.write('\r{}: {}%'.format(self.progress_message, round(self.progress_n,3)))
         else:
             sys.stdout.write('\r'+' '*(len(self.progress_message)+10)+'\r')
         sys.stdout.flush()
@@ -50,45 +54,54 @@ class TwitterJSONParse(object):
         # used for progress indicator
         self.progress_init("Formatting and extracting hashtags")
         formattedTweets = [self.progress(extractText(obj)) for obj in self.tweetObjs]
-        self.progress_init("Filtering tweets")
-        filteredTweets = [(tweet, hashtags) for (tweet, hashtags) in formattedTweets if self.progress(hashtags != [] and tweet != "")]
+        filteredTweets = [(tweet, hashtags) for (tweet, hashtags) in formattedTweets if (hashtags != [] and tweet != "")]
         # package up for retur
         tweets, hashtags = zip(*filteredTweets)
         return tweets, hashtags, Hashtag.hashtag_map
 
-def loadGlove(embeddingFile, vocabSize, embeddingDim):
+def loadGlove(embeddingFile, gloveSize, gloveDim):
     lookup = {}
     counter = 0
-    glove = np.zeros((vocabSize, embeddingDim))
+    glove = np.zeros((gloveSize, gloveDim))
     with open(embeddingFile, "r") as ef:
-        embed = ef.readline().split()
-        lookup[embed[0]] = counter
-        vect = [float(i) for i in embed[1:]]
-        glove[counter] = vect
+        for line in ef:
+            embed = line.split(' ')
+            word, *embeddingVector = embed
+            lookup[word] = counter
+            vect = [float(i) for i in embeddingVector]
+            glove[counter] = vect
+            counter += 1
     return glove, lookup
-def write_stdout(str):
-    sys.stdout.write(str)
-    sys.stdout.flush()
 
 def main():
     testFile = "data/json/cache-0-first100000.json"
     # testFile = "data/json/cache-0.json"
     gloveFile = "data/embeddings/glove.twitter.27B.25d.txt"
     vocabSize = 100000 # `wc -l <testFile>`
+    gloveSize = 1193514
     embeddingDim = 25 # must match up with glove
-    numHashtags = 500 # num most common hashtags to embed
+    numHashtags = 1000 # num most common hashtags to embed
     numHashtags_print = 100 # used in test and debug methods
 
     json = open(testFile)
     tweets, hashtags, hashtag_map = TwitterJSONParse(json, vocabSize).parseJSON()
     print("Num tweets: {}, Num unique hashtags: {}".format(len(tweets), len(hashtag_map.keys())))
+
+    print("Sorting and processing hashtags")
     # map each hashtag to the number of tweets its associated with, sort, then reverse the list
     sortedHashtags = sorted([(len(hashtag_map[key]), key) for key in hashtag_map.keys()])[-1::-1]
     justHashtagsSorted = [hashtag for _, hashtag in sortedHashtags]
     print("{} most common hashtags: {}".format(numHashtags_print, sortedHashtags[:numHashtags_print]))
 
-    glove25, glove_lookup = loadGlove(gloveFile, vocabSize, embeddingDim)
-    # blm = BaselineModel(tweets, justHashtagsSorted, hashtag_map, glove25, glove_lookup, vocabSize, embeddingDim, numHashtags)
+    print("Loading Glove Embeddings")
+    glove25, glove_lookup = loadGlove(gloveFile, gloveSize, embeddingDim)
+
+    print("Initializing BaselineModel")
+    blm = bm.BaselineModel(tweets, justHashtagsSorted, hashtag_map, glove25, gloveSize, glove_lookup, vocabSize, embeddingDim, numHashtags)
+
+    print("Initialization finished, collecting results")
+    tsne_wv, tsne_vocab = blm.hashtagEmbeddings, justHashtagsSorted[:numHashtags]
+    print("Sample Embedding: {} => {}".format(tsne_vocab[0], tsne_wv[0]))
 
 if __name__ == "__main__":
     main()